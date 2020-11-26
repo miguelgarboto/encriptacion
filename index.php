9ec07f1f86115dfa662eafd433af083e27c1dc98
miguelgarboto <miguelgarboto@gmail.com>
2020-11-26 13:11:14 +0100

    1

--- index.php
+++ index.php
@@ -1,2 +1,8 @@
 <?php
-echo md5("miguel");
+$password1 = password_hash('ana', PASSWORD_DEFAULT);
+$password2 = password_hash('pepe', PASSWORD_DEFAULT);
+$password3 = password_hash('luis', PASSWORD_DEFAULT);
+
+echo $password1.'<br>';
+echo $password2.'<br>';
+echo $password3.'<br>';
